6b0ddf92a805a049f780319d0f45e37b80005d9c
Leo Constantin <lcon69184@gmail.com>
2025-04-04 07:47:48 -0700

    added prettier for sorting of classes in tailwind

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,18 +1,18 @@
-import { allBlogs } from "content-collections";
+import { allBlogs } from "content-collections"
 
-import { Categories } from "@/constants";
+import { Categories } from "@/constants"
 
-import { Button } from "@/components/ui/button";
-import MobileCategories from "./_components/mobile-categories";
-import BlogCard from "./_components/blog_card";
-import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import MobileCategories from "./_components/mobile-categories"
+import Link from "next/link"
+import BlogCard from "./_components/blog_card"
 
 export default function page() {
   return (
     <div className="md:flex">
       <MobileCategories />
 
-      <section className="space-y-4 mr-16 hidden md:block">
+      <section className="mr-16 hidden space-y-4 md:block">
         <h1 className="text-2xl font-extrabold">Categories</h1>
         <ul className="space-y-1.5">
           {Categories.map((category, index) => (
@@ -20,9 +20,7 @@ export default function page() {
               <Button variant={"ghost"} asChild>
                 <Link
                   href={
-                    category == "all"
-                      ? "/blog"
-                      : `/blog/category/${category}`
+                    category == "all" ? "/blog" : `/blog/category/${category}`
                   }
                   className="flex items-center space-x-2 capitalize"
                 >
@@ -34,11 +32,11 @@ export default function page() {
         </ul>
       </section>
 
-      <section className="grid lg:grid-cols-2 gap-5">
+      <section className="grid gap-5 lg:grid-cols-2">
         {allBlogs.map((blog) => (
           <BlogCard key={blog.slug} {...blog} />
         ))}
       </section>
     </div>
-  );
+  )
 }
